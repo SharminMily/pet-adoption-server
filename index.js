68fc34d0a52127593ae202720a432d640d5b63ef
SharminMily <sharminmily@gmail.com>
2024-03-19 22:28:16 +0600

    donation id added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors') 
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -35,6 +35,8 @@ async function run() {
     await client.connect();
 
     const petsCollection = client.db('petsAdoption').collection('allPets');
+    const donationsCollection = client.db('petsAdoption').collection('donations');
+    const userCollection = client.db("petsAdoption").collection("users");
 
 
     // All data
@@ -44,6 +46,38 @@ async function run() {
         res.send(result);
     })
 
+    // donations
+    app.get('/donations', async(req, res) => {
+        const cursor = donationsCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+     // id
+
+     app.get("/donations/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await donationsCollection.findOne(query);
+      res.send(result)
+  })
+
+
+    // Users
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      // 1.email unique, 2.upsert,  3. simple
+
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    console.log(result)
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
